0753793ffe0e7a7a90ad01e9a71ee47608e4a478
Vrushabh Veer <veervrushabh@gmail.com>
2025-01-29 16:59:10 +0530

    added google site verification

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import ScrollToTop from "./components/common/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
